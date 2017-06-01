f413c26d6f7c0db8a5584f89375306676ad7ea28
Jack Pincus <jwpincus@gmail.com>
2017-06-01 11:21:25 -0600

    troubleshoot VCR config issue

--- spec/services/store_fetcher_spec.rb
+++ spec/services/store_fetcher_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'it can return a list of stores' do
   it 'give it a zip and it returns a prettified object for each store' do
-    VCR.use_cassette 'services/api_response' do
+    VCR.use_cassette "services/api_response" do
           new_query = StoreFetcher.new("80202")
           stores = new_query.get_store
           expect(stores.first).to respond_to(:city)
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -105,7 +105,7 @@ RSpec.configure do |config|
   config.before(:each) do
     DatabaseCleaner.start
   end
-  
+
   config.after(:each) do
     DatabaseCleaner.clean
   end
